f032e4a7952c8350261d9b15637c454e3f23ca60
Nina Shirole <ninashirole@uchicago.edu>
2021-07-15 01:48:54 +0000

    75 percent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,59 @@
 class ApplicationController < ActionController::Base
+  def add_form
+    render({ :template => "templates/add_form.html.erb" })
+  end
 
+  def calculate_add
+    @num1 = params.fetch("number_1")
+    @num2 = params.fetch("number_2")
+    @sum = @num1.to_f + @num2.to_f
+
+    render({ :template => "templates/calculate_add.html.erb" })
+  end
+
+  def subtract_form
+    render({ :template => "templates/subtract_form.html.erb" })
+  end
+
+  def calculate_subtract
+    @num1 = params.fetch("number_1").to_f
+    @num2 = params.fetch("number_2").to_f
+    @total =  @num2 - @num1
+
+    render({ :template => "templates/calculate_subtract.html.erb" })
+  end
+
+
+  def multiply_form
+    render({ :template => "templates/multiply_form.html.erb" })
+  end
+
+  def calculate_multiply
+    @num1 = params.fetch("number_1").to_f
+    @num2 = params.fetch("number_2").to_f
+    @product = @num1 * @num2
+
+    render({ :template => "templates/calculate_multiply.html.erb" })
+  end
+
+  def divide_form
+    render({ :template => "templates/divide_form.html.erb" })
+  end
+
+  def calculate_divide
+    @num1 = params.fetch("number_1").to_f
+    @num2 = params.fetch("number_2").to_f
+    @division = @num1 / @num2
+
+    render({ :template => "templates/calculate_divide.html.erb" })
+  end
+
+  
+  def street_addr_form
+    render({ :template => "templates/street_addr_form.html.erb" })
+  end
+
+  def street_addr_coords
+    render({ :template => "templates/street_addr_coords.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
+  get("/add", { :controller => "application", :action => "add_form"})
+  get("/wizard/add", { :controller => "application", :action => "calculate_add"})
+  
+  get("/subtract", { :controller => "application", :action => "subtract_form"})
+  get("/wizard/subtract", { :controller => "application", :action => "calculate_subtract"})
 
+  get("/multiply", { :controller => "application", :action => "multiply_form"})
+  get("/wizard/multiply", { :controller => "application", :action => "calculate_multiply"})
+
+  get("/divide", { :controller => "application", :action => "divide_form"})
+  get("/wizard/divide", { :controller => "application", :action => "calculate_divide"})
+  
+  get("/street_to_coords/new", { :controller => "application", :action => "street_addr_form"})
+  get("/street_to_coords/results", { :controller => "application", :action => "street_addr_coords"})
+
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
